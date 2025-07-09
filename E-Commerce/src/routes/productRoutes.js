c158ea289a5242dcb8fc4cbd8d752daeb70e72f9
nabinarai528 <np05cp4a230073@iic.edu.np>
2025-07-09 14:41:39 +0545

    Update productRoutes.js

--- E-Commerce/src/routes/productRoutes.js
+++ E-Commerce/src/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {createProduct, getAllProduct, getAllProductById} from '../controllers/productControllers.js';
+import {createProduct, deleteProductById, getAllProduct, getAllProductById, updateProduct} from '../controllers/productControllers.js';
 
 const router = express.Router()
 
 router.get('/products', getAllProduct)
 router.post('/', createProduct)
 router.get('/product/:id', getAllProductById)
+router.delete('/:id', deleteProductById)
+router.put('/:id', updateProduct)
 
 
 export default router;
