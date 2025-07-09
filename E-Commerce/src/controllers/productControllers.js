3d293e1f1427a862b3f65cbaf7a8dc0e464971f3
nabinarai528 <np05cp4a230073@iic.edu.np>
2025-07-09 14:41:03 +0545

    Update productControllers.js

--- E-Commerce/src/controllers/productControllers.js
+++ E-Commerce/src/controllers/productControllers.js
@@ -38,10 +38,38 @@ const getAllProduct = async (req, res) => {
   }
 };
 
-const getAllProductById =async (req,res) => {
-  const id = req.params.id
-  const data = await product.findById(id)
-  res.status(200).json({message: "get my singleproduct", data});
-}
+const getAllProductById = async (req, res) => {
+  const id = req.params.id;
+  const data = await product.findById(id);
+  res.status(200).json({ message: "get my singleproduct", data });
+};
+const deleteProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await product.findByIdAndDelete(id);
+    res.status(200).json({ message: "product deletes successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send("Error occured while trying to delete");
+  }
+};
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  //const { productName, ram, rom, price, description, gen, brand } = req.body;
+  try {
+    const data = await product.findByIdAndUpdate(id, req.body, { new: true });
 
-export { createProduct, getAllProduct, getAllProductById};
+    res.status(200).json({ data, message: "product updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send(error.message);
+  }
+};
+
+export {
+  createProduct,
+  getAllProduct,
+  getAllProductById,
+  deleteProductById,
+  updateProduct,
+};
