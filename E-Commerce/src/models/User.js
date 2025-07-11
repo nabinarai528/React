ad6235273202fbbd8d8eba67589e2bbb2e55909e
nabinarai528 <np05cp4a230073@iic.edu.np>
2025-07-11 13:09:35 +0545

    Create User.js

---
+++ E-Commerce/src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    role: {
+        type: String,
+        enum: ["ADMIN", "CUSTOMER"],
+        default: "CUSTOMER"
+    }
+
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema)
+export default User;
