900e126133d4f89f89b9cff6a4f4adf577b814bb
nabinarai528 <np05cp4a230073@iic.edu.np>
2025-07-14 12:35:45 +0545

    Update authRoute.js

--- E-Commerce/src/routes/authRoute.js
+++ E-Commerce/src/routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { forgotPassword, login, register } from "../controllers/authController.js";
+import generateOtp from "../config/generateOtp.js";
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+
+router.post("/forgot-password", forgotPassword)
+router.post("/generteOtp", generateOtp)
 
 
 export default router
