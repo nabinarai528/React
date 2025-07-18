fe2ec8572bd578fad3dc7366c51c4a3df8d40700
nabinarai528 <np05cp4a230073@iic.edu.np>
2025-07-18 18:58:05 +0545

    Update sendMail.js

--- E-Commerce/src/utils/sendMail.js
+++ E-Commerce/src/utils/sendMail.js
@@ -1,17 +1,20 @@
 import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
  service: "gmail",
   auth: {
-    user: "nabinarai165@gmail.com",
-    pass: "etas rofn qjcg vwnl",
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
   },
 });
 
 // Wrap in an async IIFE so we can use await.
 const sendMail = async (email,subject,message) => {
   const info = await transporter.sendMail({
-    from: '"Nabina Rai" <nabinarai165@gmail.com>',
+    from: '"Nabina Rai" <np05cp4a230073@iic.edu.np>',
     to: email,
     subject: subject,
     // text: "Hello world?", 
